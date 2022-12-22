260bf6324c2351159c7d95e61cabbf061b86970d
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-12-22 08:53:17 -0300

    Add sanityClient fetch instead api fetch

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,10 +1,12 @@
 import { Social } from "../typings";
+import { groq } from "next-sanity";
+import { sanityClient } from "../sanity";
 
 export const fetchSocials = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
-
-  const data = await res.json();
-  const socials: Social[] = data.socials;
+  const query = groq`
+*[_type == "social"]
+`;
+  const socials: Social[] = await sanityClient.fetch(query);
 
   return socials;
 };
