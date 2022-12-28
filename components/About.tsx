400ff166b9193eba7a7c598a181b6fac406492a6
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-12-28 15:32:00 -0300

    Add background resume

--- components/About.tsx
+++ components/About.tsx
@@ -64,7 +64,37 @@ function About({ pageInfo }: AboutProps) {
           viewport={{ once: true }}
           className="text-sm md:text-lg overflow-y-scroll max-h-[200px] pr-4 scrollbar-thin scrollbar-track-gray-900/50 scrollbar-track-rounded-lg scrollbar-thumb-red-600/80 hover:scrollbar-thumb-red-800/80 active:scrollbar-thumb-red-700/80 scrollbar-thumb-rounded-lg"
         >
-          {pageInfo?.backgroundInformation}
+          Olá, seja muito bem vindo (a) ao meu site! Me chamo Leandro, tenho 28
+          anos e sou bacharel em{" "}
+          <span className="underline decoration-red-600">
+            Ciência da Computação
+          </span>{" "}
+          pela Universidade Tecnológica Federal do Paraná [2015-2020].
+          Apaixonado por tecnologia, design e pelo poder que o visual pode gerar
+          por meio de produtos e serviços, venho me dedicando aos estudos da
+          area Front-end de desenvolvimento e criei este site pessoal como forma
+          de apresentar tudo aquilo em que venho trabalhando para conseguir
+          minha primeira oportunidade. <br /> <br /> Minha paixão por design
+          surgiu antes mesmo da graduação. Em 2014, trabalhei por um período
+          como{" "}
+          <span className="underline decoration-red-600">designer gráfico</span>{" "}
+          de anúncios de diversos produtos e era extremamente gratificante
+          quando conseguia desenvolver algo que agradasse aos olhos das outras
+          pessoas. Hoje, essa paixão é acentuada quando vejo sites ou
+          aplicativos que possuem um aspecto visual inovador e capaz de
+          conquistar as pessoas pelo visual. Com isso, ser capaz de
+          desenvolvê-los é o que me inspira. <br /> <br /> Venho me dedicando ao
+          estudo do ambiente de desenvolvimento web com{" "}
+          <span className="underline decoration-red-600 font-semibold">
+            HTML, CSS, JavaScript e React
+          </span>{" "}
+          como framework para agregar essas tecnologias. Me considero um
+          desenvolvedor ágil, que busca sempre entregar além do esperado e
+          principalmente "self-learning", pois tenho uma enorme capacidade de
+          aprender coisas novas por conta própria, afinal, a maior virtude de um
+          cientista é sua capacidade de pesquisa. Fique a vontade para conhecer
+          um pouco mais das minhas capacidades explorando as seções abaixo e
+          entre em contato se quiser me conhecer ainda melhor. Obrigado!
         </motion.p>
       </div>
 
