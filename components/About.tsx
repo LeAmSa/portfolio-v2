a05e184f740c1ba8bc343549a4a0b3086d5dc79e
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2023-01-03 20:51:35 -0300

    Add dynamic age

--- components/About.tsx
+++ components/About.tsx
@@ -8,6 +8,11 @@ interface AboutProps {
 }
 
 function About({ pageInfo }: AboutProps) {
+  const birth = new Date(1994, 4, 12);
+  const diff = Date.now() - birth.getTime();
+  const ageDate = new Date(diff);
+  const years = Math.abs(ageDate.getUTCFullYear() - 1970);
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-5 md:px-10 justify-center mx-auto items-center z-0">
       <SectionTitle>Sobre</SectionTitle>
@@ -64,8 +69,8 @@ function About({ pageInfo }: AboutProps) {
           viewport={{ once: true }}
           className="text-sm md:text-lg overflow-y-scroll max-h-[200px] pr-4 scrollbar-thin scrollbar-track-gray-900/50 scrollbar-track-rounded-lg scrollbar-thumb-red-600/80 hover:scrollbar-thumb-red-800/80 active:scrollbar-thumb-red-700/80 scrollbar-thumb-rounded-lg"
         >
-          Olá, seja muito bem vindo (a) ao meu site! Me chamo Leandro, tenho 28
-          anos e sou bacharel em{" "}
+          Olá, seja muito bem vindo (a) ao meu site! Me chamo Leandro, tenho{" "}
+          {years} anos e sou bacharel em{" "}
           <span className="underline decoration-red-600">
             Ciência da Computação
           </span>{" "}
@@ -74,9 +79,12 @@ function About({ pageInfo }: AboutProps) {
           por meio de produtos e serviços, venho me dedicando aos estudos da
           area Front-end de desenvolvimento e criei este site pessoal como forma
           de apresentar tudo aquilo em que venho trabalhando para conseguir
-          minha primeira oportunidade. <br /> <br /> Minha paixão por design
-          surgiu antes mesmo da graduação. Em 2014, trabalhei por um período
-          como{" "}
+          minha{" "}
+          <span className="font-semibold underline decoration-red-600">
+            primeira oportunidade
+          </span>
+          . <br /> <br /> Minha paixão por design surgiu antes mesmo da
+          graduação. Em 2014, trabalhei por um período como{" "}
           <span className="underline decoration-red-600">designer gráfico</span>{" "}
           de anúncios de diversos produtos e era extremamente gratificante
           quando conseguia desenvolver algo que agradasse aos olhos das outras
