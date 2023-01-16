2161c3171cacba8cd5c630b46f64a1b6947156df
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2023-01-16 16:20:22 -0300

    Create utils index file

---
+++ utils/index.ts
@@ -0,0 +1,6 @@
+import { fetchPageInfo } from "./fetchPageInfo";
+import { fetchProjects } from "./fetchProjects";
+import { fetchSkills } from "./fetchSkills";
+import { fetchSocials } from "./fetchSocials";
+
+export { fetchPageInfo, fetchProjects, fetchSkills, fetchSocials };
