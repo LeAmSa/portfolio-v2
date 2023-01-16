d8d2c644ad4156a084412b0628e0df0b1f6cc63f
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2023-01-16 16:19:51 -0300

    Create components index file

---
+++ components/index.tsx
@@ -0,0 +1,27 @@
+import About from "./About";
+import Alert from "./Alert";
+import BackgroundCircles from "./BackgroundCircles";
+import ContactMe from "./ContactMe";
+import Header from "./Header";
+import Hero from "./Hero";
+import Projects from "./Projects";
+import SectionTitle from "./SectionTitle";
+import Skill from "./SkillItem";
+import Skills from "./Skills";
+import Spinner from "./Spinner";
+import ToolTip from "./ToolTip";
+
+export {
+  About,
+  Alert,
+  BackgroundCircles,
+  ContactMe,
+  Header,
+  Hero,
+  Projects,
+  SectionTitle,
+  Skill,
+  Skills,
+  Spinner,
+  ToolTip,
+};
