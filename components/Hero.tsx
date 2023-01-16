7167ce6f6573bfe4ca00ac8bd4778cabf98159a5
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2023-01-16 16:22:58 -0300

    Add new layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,94 +1,101 @@
 import Image from "next/image";
-import Link from "next/link";
 
 import { motion } from "framer-motion";
 
-import {
-  UserCircleIcon,
-  BoltIcon,
-  RectangleStackIcon,
-  EnvelopeIcon,
-} from "@heroicons/react/24/solid";
-
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 
-import BackgroundCircles from "./BackgroundCircles";
-import { PageInfo } from "../typings";
+import { PageInfo, Social } from "../typings";
 import { urlFor } from "../sanity";
+import { SocialIcon } from "react-social-icons";
 
 interface HeroProps {
   pageInfo: PageInfo;
+  socials: Social[];
 }
 
-function Hero({ pageInfo }: HeroProps) {
-  const [text, count] = useTypewriter({
+function Hero({ pageInfo, socials }: HeroProps) {
+  const [text] = useTypewriter({
     words: ["Web />", "React />", "React Native />"],
     loop: true,
     delaySpeed: 2000,
   });
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-      className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden"
-    >
-      <BackgroundCircles />
-
-      <Image
-        src={urlFor(pageInfo?.heroImage).url()}
-        alt="Imagem de Perfil"
-        width={150}
-        height={150}
-        className="relative rounded-full mx-auto object-cover"
-      />
-
+    <div className="relative h-screen flex space-y-8 items-center justify-center text-center overflow-hidden max-w-7xl mx-auto z-[11] lg:translate-x-16">
       <div className="z-10">
-        <h1 className="text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-t from-gray-500/60">
+        <motion.h1
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-2xl md:text-6xl lg:text-5xl font-bold bg-clip-text lg:text-left text-transparent bg-gradient-to-t from-slate-200"
+        >
           {pageInfo?.name}
-        </h1>
+        </motion.h1>
 
-        <h2 className="text-xs md:text-sm text-gray-600 uppercase tracking-[15px]">
+        <motion.h2
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className="text-xs md:text-sm text-slate-500 lg:text-left uppercase tracking-[15px]"
+        >
           {pageInfo?.role}
-        </h2>
+        </motion.h2>
 
-        <h1 className="pt-3 text-2xl md:text-5xl font-semibold px-5 md:px-10">
+        <motion.h3
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9 }}
+          className="mt-6 md:mt-12 lg:mt-6 text-xl md:text-4xl lg:text-3xl lg:text-left font-semibold"
+        >
           <span className="mr-2 bg-clip-text text-transparent bg-gradient-to-r from-white to-red-400">
             &lt;Dev {text}
           </span>
           <Cursor cursorColor="#dc2626" />
-        </h1>
+        </motion.h3>
 
-        <div className="pt-5">
-          <Link href="#about">
-            <button className="heroButton">
-              <UserCircleIcon className="w-6 h-6 md:hidden" />
-              <span className="hidden md:block">Sobre</span>
-            </button>
-          </Link>
+        <motion.p
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.1 }}
+          className="text-slate-200 text-center lg:text-start text-base md:text-xl lg:text-lg mt-6 md:mt-12 lg:mt-6 px-12 lg:px-0"
+        >
+          Conheça a jornada de um explorador do universo front-end e amante de
+          design.
+        </motion.p>
 
-          <Link href="#skills">
-            <button className="heroButton">
-              <BoltIcon className="w-6 h-6 md:hidden" />
-              <span className="hidden md:block">Skills</span>
-            </button>
-          </Link>
-          <Link href="#projects">
-            <button className="heroButton">
-              <RectangleStackIcon className="w-6 h-6 md:hidden" />
-              <span className="hidden md:block">Projetos</span>
-            </button>
-          </Link>
-          <Link href="#contact">
-            <button className="heroButton">
-              <EnvelopeIcon className="w-6 h-6 md:hidden" />
-              <span className="hidden md:block">Contato</span>
-            </button>
-          </Link>
+        <div className="absolute bottom-[10%] left-1/2 -translate-x-1/2 lg:static lg:left-0 lg:-translate-x-0 lg:pt-20 flex justify-center lg:justify-start items-center gap-4">
+          {socials?.map((social, i) => (
+            <motion.div
+              initial={{ y: -30 - i * 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.3 + i / 10 }}
+            >
+              <SocialIcon
+                key={social._id}
+                url={social.url}
+                fgColor="gray"
+                bgColor="transparent"
+                className="hover:scale-90 transition-transform object-cover"
+              />
+            </motion.div>
+          ))}
         </div>
       </div>
-    </motion.div>
+
+      <motion.div
+        initial={{ y: -30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <Image
+          src={urlFor(pageInfo?.heroImage).url()}
+          alt="Perfil"
+          width={600}
+          height={600}
+          className="opacity-10 lg:opacity-50 lg:static absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:translate-y-0 lg:translate-x-0 w-[500px] h-[500px] lg:w-[600px] lg:h-[600px] object-cover"
+        />
+      </motion.div>
+    </div>
   );
 }
 
