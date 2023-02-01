a74f5c3ab4a1d6a0ee2c188d8a77e9ce0e9f6ecb
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2023-02-01 16:49:05 -0300

    Change text color to slate-100

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ type Props = {
 const Home = ({ pageInfo, skills, projects, socials }: Props) => {
   return (
     <div
-      className={`relative bg-[#1D2429] text-slate-200 h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-[#242424]/60 scrollbar-thumb-red-600/80 hover:scrollbar-thumb-red-800/80 active:scrollbar-thumb-red-700/80 scroll-smooth ${jost.variable} font-sans`}
+      className={`relative bg-[#1D2429] text-slate-100 h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-[#242424]/60 scrollbar-thumb-red-600/80 hover:scrollbar-thumb-red-800/80 active:scrollbar-thumb-red-700/80 scroll-smooth ${jost.variable} font-sans`}
     >
       <Head>
         <title>LeAmSa | Portfolio</title>
